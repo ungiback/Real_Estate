bce9cfa66296626a86b979903d0f0c0c51ef487c
ungi3 <ungi305@hanmail.net>
2021-12-21 06:06:22 +0900

    -

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,21 +1,23 @@
 import Head from "next/head";
 import { Box } from "@chakra-ui/react";
+import Navbar from "./Navbar";
+import Footer from "./Footer";
 
-const Layout = ({children}) =>{
-    return(
+const Layout = ({ children }) => {
+    return (
         <>
             <Head>
                 <title>Real Estate</title>
             </Head>
             <Box maxWidth="1280px" m="auto">
                 <header>
-                    Navbar
+                    <Navbar />
                 </header>
                 <main>
                     {children}
                 </main>
                 <footer>
-                    Footer
+                    <Footer />
                 </footer>
             </Box>
         </>
---
+++ components/SearchFilters.js
@@ -0,0 +1,15 @@
+import { useEffect, useState } from "react";
+import { useRouter } from "next/router";
+import { Flex, Box, Select, Tex, Input, Spinner, Icon, Button } from "@chakra-ui/react";
+import { MdCancel } from 'react-icons/md'
+import Image from "next/image";
+
+const SearchFilters = () => {
+    return(
+        <div>
+            SearchFilters
+        </div>
+    )
+}
+
+export default SearchFilters;
